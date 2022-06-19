056528af6d6e98734575cc9804cda36538620860
anitamalina <anitatranberg@gmail.com>
2022-06-19 20:52:58 +0200

    fetch api created

--- client/screens/Dashboard.js
+++ client/screens/Dashboard.js
@@ -1,13 +1,37 @@
 import { View, Text } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
+import { useEffect, useState } from "react";
 
 import styles from "./../styles";
 import CircleDiagram from "../components/CircleDiagram";
 import CategoryList from "../components/CategoryList";
 
 export default function DashBoard({ navigation }) {
-  return (
+  const [greeting, setGreeting] = useState("Hey");
+
+  useEffect(() => {
+    getGreeting();
+  }, []);
+
+  async function getGreeting() {
+    try {
+      const response = await fetch("http://localhost:8080/greeting", {
+        mode: "cors",
+      });
+      const json = await response.json();
+      setGreeting(json.content);
+      console.log(json.content);
+      console.log(greeting);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  return !greeting ? (
+    <Text>Not loaded yet.</Text>
+  ) : (
     <View style={styles.background}>
+      <Text>{greeting}</Text>
       <LinearGradient colors={["#101010", "#2B2B2B"]}>
         <Text style={styles.title1}>TOTAL WORTH</Text>
         <Text style={styles.totalworth}>778.983 DKK</Text>
