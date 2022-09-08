fd8ef857598bea7b3172400f316cb5191264de8c
anitamalina <anitatranberg@gmail.com>
2022-09-08 20:58:27 +0200

    some navigation done

--- client/App.js
+++ client/App.js
@@ -1,16 +1,13 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { useState, createContext } from "react";
-import { LinearGradient } from "expo-linear-gradient";
+
 import { StyleSheet, SafeAreaView } from "react-native";
 
 import Dashboard from "./screens/Dashboard";
 import Category from "./screens/Category";
-import Login from "./screens/Login";
-import Signup from "./screens/Signup";
-import ConfirmEmail from "./screens/ConfirmEmail";
-import ResetPassword from "./screens/ForgotPassword/ResetPassword";
-import NewPassword from "./screens/ForgotPassword/NewPassword";
+
+import Navigation from "./navigation/Navigation";
 
 export default function App() {
   const Stack = createStackNavigator();
@@ -22,13 +19,7 @@ export default function App() {
       {/* safeareaview with gradiant work around */}
       <SafeAreaView style={{ flex: 0, backgroundColor: "white" }} />
       <SafeAreaView style={styles.root}>
-        <LinearGradient colors={["#101010", "#2B2B2B"]} style={{ flex: 1 }}>
-          {/* <Signup /> */}
-          {/* <Login /> */}
-          {/* <ConfirmEmail /> */}
-          {/* <ResetPassword /> */}
-          <NewPassword />
-        </LinearGradient>
+        <Navigation />
       </SafeAreaView>
     </>
   );
