aa2608b5649973d50604570a19ce43763e878d6f
al-saffar <anders_alsaffar@hotmail.com>
2022-10-17 17:05:14 +0200

    UserController and database connection added

--- server/src/main/java/com/example/restservice/UserController.java
+++ server/src/main/java/com/example/restservice/UserController.java
@@ -1,19 +1,39 @@
 package com.example.restservice;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.concurrent.atomic.AtomicLong;
+import java.util.List;
 
-@RestController
+
+@Controller
+@RequestMapping(path = "/user")
 public class UserController {
 
+	@Autowired
+	private UserRepository userRepository;
 
-	@GetMapping("/user")
-	@CrossOrigin(origins = "http://localhost:19006/")
-	public User greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return new User("abc@gmail.com", "Anders", "And");
+	@PostMapping(path = "/add")
+	public @ResponseBody String addNewUser(@RequestParam String firstName, @RequestParam String lastName,
+										   @RequestParam String email, @RequestParam String password) {
+
+		UserEntity user = new UserEntity();
+		user.setFirstName(firstName);
+		user.setLastName(lastName);
+		user.setEmail(email);
+		user.setPassword(password);
+		userRepository.save(user);
+		return "User Created";
 	}
+
+	@GetMapping(path = "/all")
+	public @ResponseBody List<UserEntity> getAllUsers() {
+		return userRepository.findAll();
+	}
+
 }
